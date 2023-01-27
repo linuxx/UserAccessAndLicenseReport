12731f0430c8dbd3045d94ed368339dc0e5c45cf
Cris Beagle <cbeagle@gmail.com>
2023-01-27 13:03:18 -0500

    Comments, logging, download of license info from github

--- getmailboxes.ps1
+++ getmailboxes.ps1
@@ -1,19 +1,31 @@
 
 #download license map content
-$strURL = "https://raw.githubusercontent.com/linuxx/UserAccessAndLicenseReport/main/data/LicenseMap.txt"
-$objLicenseMap = Invoke-WebRequest -Uri $strURL
-$objLicenseType = $objLicenseMap.Content
+$strURL = "https://github.com/linuxx/UserAccessAndLicenseReport/raw/main/data/LicenseMap.txt"
+Invoke-WebRequest -Uri $strURL -OutFile map.json
 
-#connect to both services
+#convert to hash tables
+$objLicenses = Get-Content -raw map.json | ConvertFrom-Json 
+$objLicenseType = @{}
+$objLicenses.psobject.properties | foreach{$objLicenseType[$_.Name]= $_.Value}
+
+#connect to both services (this will prompt twice)
+Write-Host $("Connecint to Exchange Online. Look for Password Prompt...") -ForegroundColor Cyan
 Connect-ExchangeOnline
+Write-Host $("Connecint to Microsoft Online for licenses. Look for Password Prompt...") -ForegroundColor Cyan
 Connect-MsolService  
 
+#get the mailboxes
 $objMailboxes = Get-EXOMailbox | Select-Object UserPrincipalName, DisplayName, EmailAddresses
+Write-Host $("Grabbed " + $objMailboxes.count + " mailboxes") -ForegroundColor Cyan
 
+#create the output array
 $objOutput = @()
 
+#loop
 foreach($objUser in $objMailboxes)
 {
+    Write-Host $("Processing: " + $objUser.UserPrincipalName) -ForegroundColor Cyan
+    #our output object to add to the output array
     $objItem = New-Object psobject
     $objItem | Add-Member -type NoteProperty -Name 'DisplayName' -Value $objUser.DisplayName
     $objItem | Add-Member -type NoteProperty -Name 'PrimayEmail' -Value $objUser.UserPrincipalName
@@ -24,48 +36,54 @@ foreach($objUser in $objMailboxes)
     #get aliases
     foreach($strMail in $objUser.EmailAddresses)
     {
+        #if address not main mailbox
         if($strMail -cmatch 'smtp:') #case sensitive match
         {
+            #if not onmicrosoft alias
             if($strMail -cnotmatch 'onmicrosoft.com') #case sensitive match
             {
                 $strEmails += $strMail.Split(':')[1]
                 $strEmails += ","
-
+                #removes the smtp: from the address
             }
         }
     }
-    if($strEmails.Length -gt 0)
+    if($strEmails.Length -gt 0) #if we have a trailing comma from above, remove it
     {
         $strEmails = $strEmails.Substring(0, $strEmails.Length - 1)
     }
     $objItem | Add-Member -type NoteProperty -Name 'EmailAliases' -Value $strEmails
 
     #permissions
+    Write-Host $("Getting mailbox permissions...") -ForegroundColor Cyan
     $objPermissions = Get-EXOMailboxPermission -Identity $objUser.UserPrincipalName
     foreach($objPerm in $objPermissions)
     {
+        #dont need SELF, etc.
         if($objPerm.User -match '@')
         {
             $strUsersWithAccess += $objPerm.User
             $strUsersWithAccess += ','
         }
     }
-    if($strUsersWithAccess.Length -gt 0)
+    if($strUsersWithAccess.Length -gt 0) #if we have a trailing comma from above, remove it
     {
         $strUsersWithAccess = $strUsersWithAccess.Substring(0, $strUsersWithAccess.Length - 1)
     }
     $objItem | Add-Member -type NoteProperty -Name 'UsersWithAccess' -Value $strUsersWithAccess
 
     #get licenses
+    Write-Host $("Getting licenses...") -ForegroundColor Cyan
     $strLicense = ""
-    $objUserLicenses = Get-MsolUser -UserPrincipalName $objUser.UserPrincipalName | Select-Object Licenses
+    $objUserLicenses = Get-MsolUser -UserPrincipalName $objUser.UserPrincipalName -ErrorAction SilentlyContinue | Select-Object Licenses
     foreach($objLicense in $objUserLicenses.Licenses)
     {
+        #licenses are in the format of tenant:LICENSE
         $strLic = $objLicense.AccountSkuId.Split(":")
-        $strLicense += $objLicenseType[$strLic[1]]
+        $strLicense += $objLicenseType[$strLic[1]] #from the lookup table we download from github
         $strLicense += ','
     }
-    if($strLicense.Length -gt 0)
+    if($strLicense.Length -gt 0) #if we have a trailing comma from above, remove it
     {
         $strLicense = $strLicense.Substring(0, $strLicense.Length - 1)
     }
@@ -75,6 +93,8 @@ foreach($objUser in $objMailboxes)
     $objOutput += $objItem
 }
 
+#prompt for save
+Write-Host $("Done! Look for save dialog...") -ForegroundColor Cyan
 $objSaveFileDialog = New-Object windows.forms.savefiledialog
 $objSaveFileDialog.initialDirectory = [System.IO.Directory]::GetCurrentDirectory()
 $objSaveFileDialog.title = "Save User Data"
